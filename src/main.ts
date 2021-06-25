76d96e3aa0d36d107eb357aa52579b00d0d20505
Marcindev03 <marcindev03@gmail.com>
2021-06-25 18:00:05 +0200

    lesson-9

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,35 @@
-interface UserInterface {
-  getFullName(): string;
+// const updatedArray = append<string>('baz', ['foo', 'bar']);
+// const searchStr = 'foo';
+// const hasSearchedString = any<string>(
+//   (el: string) => el.contains(searchStr),
+//   ['foooo', 'bar', 'baz']
+// );
+
+const addId = <T extends object>(obj: T) => {
+  const id = Math.random().toString(16);
+
+  return {
+    ...obj,
+    id,
+  };
+};
+
+interface UserInterface<T, V> {
+  name: string;
+  data: T;
+  meta: V;
 }
 
-class User implements UserInterface {
-  protected firstName: string;
-  private lastName: string;
-  readonly age: number;
+const user: UserInterface<{ meta: string }, string> = {
+  name: 'Jack',
+  data: {
+    meta: 'foo',
+  },
+  meta: 'Some metadata',
+};
 
-  static readonly maxAge = 50;
-
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  getFullName(): string {
-    return `${this.firstName} ${this.lastName}`;
-  }
-}
-
-class Admin extends User {
-  private editor: string;
-
-  setEditor(editor: string): void {
-    this.editor = editor;
-  }
-
-  getEditor(): string {
-    return this.editor;
-  }
-}
-
-const user = new User('John', 'Doe');
+const user2: UserInterface<string[], string> = {
+  name: 'John',
+  data: ['foo', 'bar', 'baz'],
+  meta: 'Some metadata',
+};
