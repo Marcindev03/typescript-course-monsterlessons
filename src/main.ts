ce47b9eaccd73d72aaa9bf8fc0237c11eb5f3c94
Marcindev03 <marcindev03@gmail.com>
2021-06-25 17:03:47 +0200

    lesson-7

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
-const doSomething = (): void => {
-    console.log('Hello world');
-};
-
-const someFunction = (): never => {
-    throw 'never';
-};
-
 let vAny: any = 10;
 let vUnknown: unknown = 10;
 
 let s1: string = vAny;
-// let s2: string = vUnknown;
 
-console.log(vAny.foo());
+// Type assertion
+let s2: string = vUnknown as string;
+
+let pageNumber: string = '1';
+let numericPageNumber: number = pageNumber as unknown as number;
+
+// console.log(vAny.foo());
 // console.log(vUnknown.foo());
+
+
 
